45a8f5e1d4e38745788216eab573a620e0d17892
Javier <202004890@alu.comillas.edu>
2024-02-27 09:47:38 +0100

    p3

---
+++ src/variables.js
@@ -0,0 +1,17 @@
+const booleano = true && !false || true;
+const numero = 10 + 2 * 2 / 4 - 1;
+const cadena = 'Cadena con \' (comilla simple escapada)';
+const cadenaConVariables = `Núm:${numero}, Booleano:${booleano}, Cadena:'${cadena}'`;
+
+// Comentario: ¡pinta por consola para depurar!
+// console.log(cadenaConVariables);
+
+// console.log((12/0)>(10/0));
+
+// console.log(1+'1'+true);
+
+let chain = 'un dos tres';
+let palabras = chain.split(' ');
+console.log(palabras)
+
+
